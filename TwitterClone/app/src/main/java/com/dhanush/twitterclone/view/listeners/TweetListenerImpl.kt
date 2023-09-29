a07333615a5ff90e1e0ae23f1f0f9ce6ac45adae
DhanushP <dhanushpakanati@gmail.com>
2023-09-28 19:58:12 -0500

    Added twitter listener implementation to implement twitterlistener interface

---
+++ TwitterClone/app/src/main/java/com/dhanush/twitterclone/view/listeners/TweetListenerImpl.kt
@@ -0,0 +1,17 @@
+package com.dhanush.twitterclone.view.listeners
+
+import com.dhanush.twitterclone.model.Tweet
+
+class TweetListenerImpl: TweetListener {
+    override fun onLayoutClick(tweet: Tweet?) {
+
+    }
+
+    override fun onLike(tweet: Tweet?) {
+
+    }
+
+    override fun onRetweet(tweet: Tweet?) {
+
+    }
+}
