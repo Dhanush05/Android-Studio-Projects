6b4158ce63688ad7e46f70d8793c98fde0b27612
DhanushP <dhanushpakanati@gmail.com>
2023-10-12 16:52:07 -0500

    Initial files
    Added permissions for internet in manifest

--- TwitterClone/app/src/main/java/com/dhanush/twitterclone/view/listeners/TweetListenerImpl.kt
+++ TwitterClone/app/src/main/java/com/dhanush/twitterclone/view/listeners/TweetListenerImpl.kt
@@ -1,8 +1,12 @@
 package com.dhanush.twitterclone.view.listeners
 
+import androidx.recyclerview.widget.RecyclerView
 import com.dhanush.twitterclone.model.Tweet
+import com.dhanush.twitterclone.model.User
+import com.dhanush.twitterclone.viewmodel.SharedViewModel
 
-class TweetListenerImpl: TweetListener {
+class TweetListenerImpl(val tweetList: RecyclerView, var user: User?, val sharedViewModel: SharedViewModel ): TweetListener {
+
     override fun onLayoutClick(tweet: Tweet?) {
 
     }
