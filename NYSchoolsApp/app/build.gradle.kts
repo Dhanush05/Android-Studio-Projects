0ff4456ad0874b971a827c18c9898188ca139fde
DhanushP <dhanushpakanati@gmail.com>
2023-10-12 17:09:31 -0500

    Dependencies
    Dagger, Retrofit, rx, glide

--- NYSchoolsApp/app/build.gradle.kts
+++ NYSchoolsApp/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+
 }
 
 android {
@@ -37,10 +39,33 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+//dagger dependencies
+    implementation("com.google.dagger:dagger:2.47") // Dagger core library
+    kapt("com.google.dagger:dagger-compiler:2.47")
+    implementation("com.google.dagger:dagger-android:2.47") // Dagger Android support
+    implementation("com.google.dagger:dagger-android-support:2.47") // Dagger Android support for fragments and activities
+    kapt("com.google.dagger:dagger-android-processor:2.47") // Dagger Android processor
+//retrofit dependencies
+    implementation ("com.squareup.retrofit2:retrofit: 2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
+//rxjava dependencies
+    implementation ("io.reactivex.rxjava2:rxjava:2.2.21")
+    implementation ("io.reactivex.rxjava2:rxandroid:2.1.1")
+
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
+//glide
+    implementation("com.github.bumptech.glide:glide:4.16.0") // Glide dependency
+    kapt("com.github.bumptech.glide:compiler:4.16.0") // Glide annotation processor
+
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0") // Lifecycle Extensions dependency
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
