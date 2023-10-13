9597d94d5a8b00be936b4721c5403bfe9944998c
DhanushP <dhanushpakanati@gmail.com>
2023-10-12 21:03:21 -0500

    Recycler view displays list of all schools

--- NYSchoolsApp/app/src/main/java/com/dhanush/nyschoolsapp/view/MainActivity.kt
+++ NYSchoolsApp/app/src/main/java/com/dhanush/nyschoolsapp/view/MainActivity.kt
@@ -2,16 +2,34 @@ package com.dhanush.nyschoolsapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dhanush.nyschoolsapp.R
 import com.dhanush.nyschoolsapp.databinding.ActivityMainBinding
+import com.dhanush.nyschoolsapp.viewmodel.ListViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var schoolsAdapter = SchoolsListAdapter(arrayListOf())
-
+    lateinit var viewModel : ListViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding  = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel = ViewModelProvider(this)[ListViewModel::class.java]
+        viewModel.refresh()
+        binding.listview.apply{
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = schoolsAdapter
+        }
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.schools.observe(this,{
+            it?.let{
+                schoolsAdapter.updateSchools(it)
+            }
+        })
     }
 }
--- NYSchoolsApp/app/src/main/java/com/dhanush/nyschoolsapp/viewmodel/ListViewModel.kt
+++ NYSchoolsApp/app/src/main/java/com/dhanush/nyschoolsapp/viewmodel/ListViewModel.kt
@@ -1,14 +1,48 @@
 package com.dhanush.nyschoolsapp.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.dhanush.nyschoolsapp.di.DaggerApiComponent
 import com.dhanush.nyschoolsapp.model.School
+import com.dhanush.nyschoolsapp.model.SchoolsService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
-class ListViewModel {
+class ListViewModel: ViewModel() {
     val schools = MutableLiveData<List<School>>()
     val schoolLoadError = MutableLiveData<Boolean>()
+    private val disposable = CompositeDisposable()
     val loading = MutableLiveData<Boolean>()
-    private fun fetchSchools(){
+    @Inject
+    lateinit var schoolsService: SchoolsService
+    init{
+        DaggerApiComponent.create().inject(this)
+    }
 
+    fun refresh(){
+        fetchSchools()
+    }
+    private fun fetchSchools(){
+        disposable.add(schoolsService.getSchools()
+            .subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(object : DisposableSingleObserver<List<School>>(){
+                override fun onSuccess(t: List<School>) {
+                    schools.value = t
+                    schoolLoadError.value = false
+                }
+                override fun onError(e: Throwable) {
+                    schoolLoadError.value  =true
+                }
+            }))
+
+    }
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
     }
 
 }
