bb700579c49feb4536e0291b31cf05842aa5a915
DhanushP <dhanushpakanati@gmail.com>
2023-11-17 21:13:08 -0600

    Added insertions, deletion and query in DAO.

--- RunningApp/app/src/main/java/com/dhanush/runningapp/db/Run.kt
+++ RunningApp/app/src/main/java/com/dhanush/runningapp/db/Run.kt
@@ -1,3 +1,18 @@
 package com.dhanush.runningapp.db
 
-data class Run()
+import android.graphics.Bitmap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "running_table")
+data class Run(
+    var img: Bitmap? = null, //when the run was started
+    var timestamp: Long = 0L,
+    var avgSpeedInKMH: Float = 0f,
+    var distanceInMeters: Int = 0,
+    var timeInMillis: Long = 0L, //how long the run was
+    var caloriesBurned: Int = 0
+){
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+}
